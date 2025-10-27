170d8f8c950aef739316b3eef4f75635806fdc28
Edwin-Cortez <ecortezt@miumg.edu.gt>
2025-10-26 21:04:22 -0600

    Add files via upload

---
+++ ProductoService (1).java
@@ -0,0 +1,47 @@
+package com.example.productos.service;
+
+import com.example.productos.model.Producto;
+import com.example.productos.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> listarTodos() {
+        return productoRepository.findAll();
+    }
+
+    public Optional<Producto> obtenerPorId(Long id) {
+        return productoRepository.findById(id);
+    }
+
+    public Producto crear(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public Optional<Producto> actualizar(Long id, Producto productoActualizado) {
+        return productoRepository.findById(id)
+                .map(producto -> {
+                    producto.setNombre(productoActualizado.getNombre());
+                    producto.setDescripcion(productoActualizado.getDescripcion());
+                    producto.setPrecio(productoActualizado.getPrecio());
+                    producto.setStock(productoActualizado.getStock());
+                    return productoRepository.save(producto);
+                });
+    }
+
+    public boolean eliminar(Long id) {
+        if (productoRepository.existsById(id)) {
+            productoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
